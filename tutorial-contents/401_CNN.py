f4ee12c84be19b136fcd8cd54003fe7334f7e046
Morvan Zhou <morvanzhou@gmail.com>
2017-05-11 00:34:30 +1000

    update

--- tutorial-contents/401_CNN.py
+++ tutorial-contents/401_CNN.py
@@ -15,7 +15,6 @@ import numpy as np
 tf.set_random_seed(1)
 np.random.seed(1)
 
-EPOCH = 1               # train the training data n times, to save time, we just train 1 epoch
 BATCH_SIZE = 50
 LR = 0.001              # learning rate
 
@@ -24,7 +23,7 @@ test_x = mnist.test.images[:2000]
 test_y = mnist.test.labels[:2000]
 
 tf_x = tf.placeholder(tf.float32, [None, 28*28])/255.  # normalize to range (0, 1)
-image = tf.reshape(tf_x, [-1, 28, 28, 1])                   # (batch, height, width, channel)
+image = tf.reshape(tf_x, [-1, 28, 28, 1])              # (batch, height, width, channel)
 tf_y = tf.placeholder(tf.int32, [None, 10])            # input y
 
 # CNN
@@ -35,16 +34,16 @@ conv1 = tf.layers.conv2d(   # shape (28, 28, 1)
     strides=1,
     padding='same',
     activation=tf.nn.relu
-)          # -> (28, 28, 16)
+)           # -> (28, 28, 16)
 pool1 = tf.layers.max_pooling2d(
     conv1,
     pool_size=2,
     strides=2,
-)   # -> (14, 14, 16)
+)           # -> (14, 14, 16)
 conv2 = tf.layers.conv2d(pool1, 32, 5, 1, 'same', activation=tf.nn.relu)    # -> (14, 14, 32)
 pool2 = tf.layers.max_pooling2d(conv2, 2, 2)    # -> (7, 7, 32)
-flat = tf.reshape(pool2, [-1, 7*7*32])    # -> (7*7*32, )
-output = tf.layers.dense(flat, 10)        # output layer
+flat = tf.reshape(pool2, [-1, 7*7*32])          # -> (7*7*32, )
+output = tf.layers.dense(flat, 10)              # output layer
 
 loss = tf.losses.softmax_cross_entropy(onehot_labels=tf_y, logits=output)           # compute cost
 train_op = tf.train.AdamOptimizer(LR).minimize(loss)
@@ -52,10 +51,9 @@ train_op = tf.train.AdamOptimizer(LR).minimize(loss)
 accuracy = tf.metrics.accuracy(          # return (acc, update_op), and create 2 local variables
     labels=tf.argmax(tf_y, axis=1), predictions=tf.argmax(output, axis=1),)[1]
 
-sess = tf.Session()                                                                 # control training and others
+sess = tf.Session()
 init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()) # the local var is for accuracy_op
 sess.run(init_op)     # initialize var in graph
-
 
 for step in range(600):
     b_x, b_y = mnist.train.next_batch(BATCH_SIZE)
